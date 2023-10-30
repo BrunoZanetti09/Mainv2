2a3aebd6667bc765e698a799e54fbe8503145b25
Bruno Zanetti <bzanetti09@gmail.com>
2023-10-30 20:20:26 -0300

    Main

---
+++ app/models/user.rb
@@ -0,0 +1,45 @@
+class User < ApplicationRecord
+  has_secure_password
+  
+  has_one :profile, dependent: :destroy
+
+  after_create :set_profile
+
+  def set_profile
+  self.profile = profile.create()
+  end
+
+  validates :email, presence: true, uniqueness: true,
+  format: { with: /\A([\w+-].?)+@[a-z\d-]+(.[a-z]+)*.[a-z]+\z/i,
+    message: "formato del mail invalido"
+  }
+
+  validates :dni, 
+  presence: true,
+  uniqueness: true,
+  format: {
+    with: /\A[0-9]+\z/,
+    message: "debe tener solo números"
+  }
+
+  validates :password, presence: true
+  validates :nombre, presence: true,
+  format: {
+    with: /\A[a-z-A-Z]+\z/,
+    message: "solo se permiten letras en el nombre"
+  }
+  validates :apellido, presence: true,
+  format: {
+      with: /\A[a-z-A-Z]+\z/,
+      message: "solo se permiten letras en el apellido"
+    }
+  validates :rol, presence: true
+
+  before_save :downcase_attributes
+
+  private
+
+  def downcase_attributes
+      self.email = email.downcase
+  end
+end
